fc4c043a856c1f2321b4008d783ec1ad2225b797
Franco Albornoz <albfrank12@gmail.com>
2021-11-07 12:48:20 -0300

    Objetos y array

--- pages/simulador.js
+++ pages/simulador.js
@@ -1,4 +1,4 @@
-
+alert("BIENVENIDO AL SIMULADOR DE COMPRA")
 alert("Producto 1: $3500\n Producto 2: $4500\n Producto 3: $3500\n Producto 4: $4000\n Producto 5: $8500\n Producto 6: $9500\n")
 let productoN=Number(prompt("Ingrese el producto que desea comprar. 0 Para finalizar\n Producto 1:Teclado MarvoScorpion \nProducto 2: Mouse Logitech MX Anyw \nProducto 3: Mouse XPGamer \nProducto 4: Auriculares Gamemax G200 \nProducto 5: Microfono HyperX QuadCast \nProducto 6: Teclado Redragon K552 Kumara"));
 
@@ -12,10 +12,36 @@ class Producto{
 
 	}
 
-	/*sumaTot(){
-		this.precio=this.precio+
-	}*/
+	
 }
+
+/*function sumar(){
+
+	precioT=precioT+precioP
+
+}
+
+function desc(){
+	if(precioT>=30000){
+		precioT=precioT*0.15
+		alert("Recibiras un descuento del 15%. Gracias")
+	}
+
+}
+
+
+function promo(){
+	if(precioT>=30000){
+		desc()
+		alert("Recibe promocion");
+	}
+	else{
+		alert("No alcanza para la promocion");
+	}
+}*/
+
+const cart= [];
+
 
 const producto1 = new Producto(1, 'Teclado MarvoScorpion',3500, 12)
 const producto2 = new Producto(2, 'Mouse Logitech MX Anyw',4500, 5)
@@ -30,33 +56,40 @@ while(productoN != 0){
 	switch(productoN){
 		case 1:
 			
-			console.log(producto1)
+			//console.log(producto1)
 			cart.push(producto1)
+			break
 			
 		case 2:
 			
-			console.log(producto2)
+			//console.log(producto2)
 			cart.push(producto2)
+			break
+
 			
 		case 3:
 			
-			console.log(producto3)
+			//console.log(producto3)
 			cart.push(producto3)
+			break
 			
 		case 4:
 			
-			console.log(producto4)
+			//console.log(producto4)
 			cart.push(producto4)
+			break
 
 		case 5:
 			
-			console.log(producto5)
+			//console.log(producto5)
 			cart.push(producto5)
+			break
 			
 		case 6:
 
-			console.log(producto6)
+			//console.log(producto6)
 			cart.push(producto6)
+			break
 
 		default:
 
@@ -69,7 +102,6 @@ while(productoN != 0){
 	productoN=Number(prompt("Ingrese el producto que desea comprar. 0 Para finalizar"));
 }
 
-const cart= [];
 console.log(cart)
 
 
