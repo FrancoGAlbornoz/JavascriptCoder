bf532071c1c83356a6d7a9c77826a17f8e01e263
Franco Albornoz <albfrank12@gmail.com>
2021-11-15 11:10:13 -0300

    Arrays y simulador Entrega

---
+++ pages/funcionesSimulador.js
@@ -0,0 +1,53 @@
+
+//Funcion del menu
+const showMenu = () => {
+
+    let menu= "Por favor elige tu producto\n"
+    products.forEach((product)=>{
+        menu += product.id + " - " + product.name +"\n"
+    })
+    //.length longitud de un array, en este caso la longuitud del array products
+    menu+= (products.length+1)+".-Salir"
+    // El usuario ingresaa un caracter numero, lo transforma en int y lo guarda en option, y lo retorna(ID DEL PRODUCTO) a la funcion que lo llame
+    let option =parseInt(prompt(menu))
+    return option
+
+}
+
+const isStock = (qty, stock) => {
+    if(qty > stock){
+        alert(`No hay stock. Stock disponible  ${stock}`)
+        return false
+    }
+    return true
+}
+
+
+const addCart = (option, qty) => {
+    //busco el producto que tenga el mismo valor que la variable option
+    const found=products.find(product => product.id == option)
+    if(isStock(qty, found.stock)){
+        let item= {
+            qty: qty,
+            price: found.price*qty,
+            name: found.name
+
+        }
+        cart.push(item)
+        products[option -1 ].stock-=qty
+        alert(`${found.name} Agregados`)
+    }
+  
+}
+
+/*const ingresado= prompt("Ingrese el producto que desea buscar")
+const prodIng= products.filter(product=>product.name.includes(ingresado))
+document.write("<h4>Producto buscado. </h4>")
+
+for (var products of prodIng){
+    document.write("<h4> Nombre del producto buscado. </h4>")*/
+}
+
+
+item={}
+
--- pages/simulador.js
+++ pages/simulador.js
@@ -1,123 +1,37 @@
-alert("BIENVENIDO AL SIMULADOR DE COMPRA")
-alert("Producto 1: $3500\n Producto 2: $4500\n Producto 3: $3500\n Producto 4: $4000\n Producto 5: $8500\n Producto 6: $9500\n")
-let productoN=Number(prompt("Ingrese el producto que desea comprar. 0 Para finalizar\n Producto 1:Teclado MarvoScorpion \nProducto 2: Mouse Logitech MX Anyw \nProducto 3: Mouse XPGamer \nProducto 4: Auriculares Gamemax G200 \nProducto 5: Microfono HyperX QuadCast \nProducto 6: Teclado Redragon K552 Kumara"));
+let resp='si'
+
+const selectedQty = 'Cantidad'
+
+let cart= []
 
 
-class Producto{
-	constructor (id, name, price, stock){
-		this.id=id
-		this.name=name
-		this.price=price
-		this.stock=stock
+let products= [
 
-	}
+    {id:1 , name:'Teclado MarvoScorpion', price:3500 , stock:12},
+    {id:2 , name:'Mouse Logitech MX Anyw', price:4500 , stock:5},
+    {id:3 , name:'Mouse XPGamer', price:3500 , stock:10},
+    {id:4 , name:'Auriculares Gamemax G200', price:4000 , stock:9},
+    {id:5 , name:'Microfono HyperX QuadCast', price:8500 , stock:10},
+    {id:6 , name:'Teclado Redragon K552 Kumara', price:9000 , stock:8},
+    
+]
 
-	
-}
+alert("Bienvenido a E-Tienda")
 
-/*function sumar(){
+do{
 
-	precioT=precioT+precioP
-
-}
-
-function desc(){
-	if(precioT>=30000){
-		precioT=precioT*0.15
-		alert("Recibiras un descuento del 15%. Gracias")
-	}
-
-}
-
-
-function promo(){
-	if(precioT>=30000){
-		desc()
-		alert("Recibe promocion");
-	}
-	else{
-		alert("No alcanza para la promocion");
-	}
-}*/
-
-const cart= [];
-
-
-const producto1 = new Producto(1, 'Teclado MarvoScorpion',3500, 12)
-const producto2 = new Producto(2, 'Mouse Logitech MX Anyw',4500, 5)
-const producto3 = new Producto(3, 'Mouse XPGamer',3500, 2)
-const producto4 = new Producto(4, 'Auriculares Gamemax G200',4000, 10)
-const producto5 = new Producto(5, 'Microfono HyperX QuadCast',8500, 15)
-const producto6 = new Producto(6, 'Teclado Redragon K552 Kumara',9500, 8)
-
-
-while(productoN != 0){
-
-	switch(productoN){
-		case 1:
-			
-			//console.log(producto1)
-			cart.push(producto1)
-			break
-			
-		case 2:
-			
-			//console.log(producto2)
-			cart.push(producto2)
-			break
-
-			
-		case 3:
-			
-			//console.log(producto3)
-			cart.push(producto3)
-			break
-			
-		case 4:
-			
-			//console.log(producto4)
-			cart.push(producto4)
-			break
-
-		case 5:
-			
-			//console.log(producto5)
-			cart.push(producto5)
-			break
-			
-		case 6:
-
-			//console.log(producto6)
-			cart.push(producto6)
-			break
-
-		default:
-
-			alert("Ingrese su opcion")
-
-	}
-
-
-	
-	productoN=Number(prompt("Ingrese el producto que desea comprar. 0 Para finalizar"));
-}
+    let option =showMenu()
+    if(option===(products.length+1)){
+        break
+    } 
+    let qty = parseInt(prompt(selectedQty))
+    addCart(option,qty)
+    
+    res= prompt("Desea continuar agregando productos al carrito? si/no \n")
+    
+}while(resp=== 'si')
 
 console.log(cart)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
