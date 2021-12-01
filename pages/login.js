a63e180bd11d409f3fc5b7f79069b07ad3ca2cbd
Franco Albornoz <albfrank12@gmail.com>
2021-12-01 12:28:26 -0300

    DOM

--- pages/login.js
+++ pages/login.js
@@ -1,67 +1,100 @@
-class User{
+/*class User{
     constructor(nombre, apellido , pass , email){
         this.nombre= nombre
-        this.apellido = this.apellido
+        this.apellido = apellido
         this.pass = pass
         this.email = email
     }
 
+}*/
+
+function getLocalStorage(key) {
+
+    return JSON.parse(localStorage.getItem(key))
+}
+
+function saveInLocalStorage(key, item) {
+
+    let stringifiedItem = JSON.stringify(item)
+    localStorage.setItem(key, stringifiedItem)
+
 }
 
 
-/*let users = []
-let user= {
+let users = []
+console.log(users)
 
-    nombre: nombre.value,
-    apellido: apellido.value,
-    mail: mail.value,
-    pass: pass.value,
 
-}
-users.push(user)*/
+let nombre = document.querySelector('#nombreUs')
+nombre.addEventListener('keyup', function () {
 
-function registrar(){
-    const nombre = document.getElementById("nombreUs")
-    const apellido = document.getElementById("apellidoUs")
-    const pass = document.getElementById("passUs")
-    const mail = document.getElementById("mailUs")
+    console.log(nombre.value)
+ 
+ })
+
+let apellido = document.querySelector('#apellidoUs')
+apellido.addEventListener('keyup', function () {
+
+    console.log(apellido.value)
+
+})
+
+let mail = document.querySelector('#mailUs')
+mail.addEventListener('keyup', function () {
+
+    console.log(Number(mail.value))
+
+})
+
+let pass = document.querySelector('#passUs')
+pass.addEventListener('keyup', function () {
+
+    console.log(Number(pass.value))
+
+})
+
+let btn = document.querySelector("#Registrarme")
+
+btn.addEventListener('click', function (event){
+    event.preventDefault()
+
+    let user= {
+
+        nombre: nombre.value,
+        apellido: apellido.value,
+        mail: mail.value,
+        pass: pass.value,
+    
+    }
+    users.push(user)
 
     const mensaje= document.getElementById("mensaje")
-
+    
     if(nombre.value==nombre && pass.value==pass){
-
+    
         console.log("B")
         const welcome =document.createElement("p")
         welcome.innerHTML="Bienvenido"
         welcome.style.color = "red"
         welcome.style.fontSize = "40px"
-    
+
         mensaje.appendChild(welcome)
-
-
+    
     }else{
         console.log("Error")
     }
     
-
-    console.log(usuario.value);
-    //console.log(users)
+    console.log(nombre.value);
     
 
-}
+    
+})
 
 
-function template(){
-
-
-    let mensaje = document.getElementById("mensaje");
+let mensaje = document.getElementById("mensaje");
     mensaje.innerHTML = `<h2>Datos del usuario</h2>
-                         <p>Nombre del usuario: ${nombre}</p>
-                         
-                         <p>Email del usuario: ${email}</p>                       
-    `;
-
-}
-
-
+                        <p>Nombre del usuario: ${nombre.value}</p>
+                             
+                        <p>Email del usuario: ${mail.value}</p>                       
+        `;
 
